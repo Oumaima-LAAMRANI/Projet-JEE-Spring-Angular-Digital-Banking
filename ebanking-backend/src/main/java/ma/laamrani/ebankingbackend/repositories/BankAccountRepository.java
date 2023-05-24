be63d944ad44a765af460eb55aae470f8eabd63c
Oumaima-LAAMRANI <oumaima.laamrani.2002@gmail.com>
2023-05-25 00:01:34 +0200

    Création des interfaces JPA Repository basées sur Spring Data

--- ebanking-backend/src/main/java/ma/laamrani/ebankingbackend/repositories/BankAccountRepository.java
+++ ebanking-backend/src/main/java/ma/laamrani/ebankingbackend/repositories/BankAccountRepository.java
@@ -1,8 +1,5 @@
 package ma.laamrani.ebankingbackend.repositories;
-
-
 import ma.laamrani.ebankingbackend.entities.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
 }
