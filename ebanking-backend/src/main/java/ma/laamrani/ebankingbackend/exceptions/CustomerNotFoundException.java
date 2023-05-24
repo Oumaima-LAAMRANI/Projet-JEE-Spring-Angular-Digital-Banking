6c76ef1795eb63f5c469c1126bf65afc045f9c5a
Oumaima-LAAMRANI <oumaima.laamrani.2002@gmail.com>
2023-05-25 01:38:53 +0200

    Ajout des exceptions

---
+++ ebanking-backend/src/main/java/ma/laamrani/ebankingbackend/exceptions/CustomerNotFoundException.java
@@ -0,0 +1,6 @@
+package ma.laamrani.ebankingbackend.exceptions;
+public class CustomerNotFoundException extends Exception {
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
